2ff20f8985819df80e572fcbe9718d12b6875abd
Vlen2R <valentin2romero@gmail.com>
2020-10-03 19:44:25 -0300

    Actualizando whishlist

--- angular-whishlist/src/app/app.module.ts
+++ angular-whishlist/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinoComponent } from './lista-destino/lista-destino.component';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
+
+const routes: Routes=[
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+  {path:'home', component: ListaDestinoComponent},
+  {path: 'destino', component: DestinoDetalleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
-    ListaDestinoComponent
+    ListaDestinoComponent,
+    DestinoDetalleComponent,
+    FormDestinoViajeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-destino-viaje',
+  templateUrl: './form-destino-viaje.component.html',
+  styleUrls: ['./form-destino-viaje.component.css']
+})
+export class FormDestinoViajeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
