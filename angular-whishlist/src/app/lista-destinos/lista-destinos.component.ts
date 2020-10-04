cb49c542666af9362fb5e1cba3287b1e110f6421
Vlen2R <valentin2romero@gmail.com>
2020-10-03 23:51:40 -0300

    Semana 2 - Leccion 3 - Video 1 - Principios de diseñeo de observables y RxJ5

--- angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { DestinoViaje } from './../models/destino-viaje.model';
 import { DestinosApiClient } from './../models/destinos-api-client.model';
-
 @Component({
   selector: 'app-lista-destinos',
   templateUrl: './lista-destinos.component.html',
@@ -9,21 +8,26 @@ import { DestinosApiClient } from './../models/destinos-api-client.model';
 })
 export class ListaDestinosComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
+  updates:DestinoViaje[];
 
-  constructor(public destinosApiClient:DestinosApiClient) {
+  constructor(private destinosApiClient:DestinosApiClient) {
     this.onItemAdded = new EventEmitter();
+    this.updates = [];
+    this.destinosApiClient.subscribeOnChange((d:DestinoViaje) => {
+        if (d != null) {
+          this.updates.push(d);
+        }
+      });
   }
 
   ngOnInit() {
   }
-
   agregado(d:DestinoViaje) {
     this.destinosApiClient.add(d);
     this.onItemAdded.emit(d);
   }
 
-  elegido(e:DestinoViaje) {
-    this.destinosApiClient.getAll().forEach(x => x.setSelected(false));
-    e.setSelected(true);
+  elegido(d:DestinoViaje) {
+    this.destinosApiClient.elegir(d);
   }
 }
